ac2bc6677911262d7f8db700449b00eec0fc39a7
Louis Lambeau <llambeau@enspirit.be>
2022-10-19 20:28:26 +0200

    WIP

--- src/drivers/wk.rs
+++ src/drivers/wk.rs
@@ -1,10 +1,20 @@
-use wkhtmltopdf;
+use std::io::Read;
+
+use wkhtmltopdf::*;
 use super::HtmlToPdfWriter;
 
 pub struct WkHtmlToPdf;
 
 impl HtmlToPdfWriter for WkHtmlToPdf {
-  fn html_to_pdf(&self, _html: &str) {
+  fn html_to_pdf(&self, html: &str) {
+
+    // let pdf_app = PdfApplication::new().expect("Failed to init PDF application");
+    // let mut pdfout = pdf_app.builder()
+    //   .orientation(Orientation::Landscape)
+    //   .margin(Size::Inches(2))
+    //   .title("Awesome Foo")
+    //   .build_from_html(html);
+
     println!("Building html with WK");
   }
 }
